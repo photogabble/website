de6f5b822c168598b09decb959e43f1dd5c9bf74
Simon Dann <simon.dann@gmail.com>
2021-02-21 08:43:27 +0000

    wip :: laying out the various page types, fun!

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
         "./_tmp/style.css": "./style.css",
         "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+        "./images" : './images'
     });
     eleventyConfig.addShortcode("version", function () {
         return String(Date.now());
