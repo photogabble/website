f2f86567b67fcdf29feb102c0e44cebae2a4e492
Simon Dann <simon.dann@gmail.com>
2021-03-05 21:48:08 +0000

    wip :: general tinkering

---
+++ _data/links.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        title: 'WebAssembly Search Tools for Static Sites',
+        href: 'https://healeycodes.com/webassembly-search-tools-for-static-websites/',
+        tags: ['wasm', 'go'],
+        reason: '...',
+    },
+    {
+        title: 'Magazine Website Theme',
+        href: 'https://magazine.blot.im/',
+        tags: ['design'],
+        reason: '...',
+    },
+    {
+        title: 'Import Tweets from Twitter API in 11ty',
+        href: 'https://www.d-hagemeier.com/en/import-tweets-from-twitter-api-in-11ty/',
+        tags: ['js', '11ty'],
+        reason: '...',
+    }
+];
