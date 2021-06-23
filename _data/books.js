0a217738b2d9149317c8a5ff3476e43c0a7abc14
Simon Dann <simon.dann@achiive.co.uk>
2021-06-23 21:52:44 +0100

    feat :: added books page using integration with bookwyrm.social

---
+++ _data/books.js
@@ -0,0 +1,74 @@
+const fetch = require("node-fetch");
+
+const username = 'carbontwelve';
+
+module.exports = async function() {
+  console.log(`Fetching bookwyrm feed for [${username}]…`);
+
+  const numberOfPages = await fetch(`https://bookwyrm.social/user/${username}/outbox`)
+    .then(res => res.json())
+    .then(json => {
+      if (json.first === json.last) {
+        return 1;
+      }
+
+      let n = 1;
+    })
+
+  /**
+   * @param page {Number}
+   * @return {Array}
+   */
+  const fetchPage = async (page) => {
+    return await fetch(`https://bookwyrm.social/user/${username}/outbox?page=${page}`)
+      .then(res => res.json())
+      .then(json => {
+        if (json.type === 'OrderedCollectionPage' && json.orderedItems.length > 0) {
+          return json.orderedItems.filter(item => item.type === 'Article');
+        }
+        return [];
+      })
+      .then(items => items.map(async item => {
+        const book = await fetch(item.inReplyToBook + '.json').then(res => res.json()).then(edition => {
+          const authors = edition.authors.map(author => {
+            return fetch(author + '.json').then(res => res.json()).then(author => author.name)
+          })
+
+          const cover = edition.cover ?
+            {
+              src: edition.cover.url,
+              alt: edition.cover.name
+            } : null;
+
+          return Promise.all(authors).then((authors) => {
+            return {
+              id: edition.id,
+              title: edition.title,
+              authors: authors,
+              publishedDate: edition.publishedDate,
+              review: item.content,
+              cover
+            }
+          })
+        })
+        return {
+          id: item.id,
+          published: new Date(item.published),
+          rating: item.rating,
+          content: item.content,
+          book
+        }
+      }))
+  }
+
+  let books = [];
+
+  for (let page = 1; page <= numberOfPages; page++) {
+    console.log(`Fetching bookwyrm feed for [${username}] page ${page} of ${numberOfPages}…`);
+    const items = await fetchPage(page);
+    items.every(item => books.push(item));
+  }
+
+  return Promise.all(books);
+
+};
--- books.md
+++
@@ -1,7 +0,0 @@
----
-layout: layouts/page.njk
-title: "Books"
-sub_title: "Last updated March 19th, 2021."
----
-
-Make this a page much like [Cox Chapman's Books page](https://www.coxchapman.com/books), the source repository can be found [here](https://github.com/coxchapman/coxchapman.github.io).
