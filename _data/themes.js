34424d8955c7d7f6f79a91cad9e495aee92755f3
Simon Dann <carbontwelve@users.noreply.github.com>
2022-01-31 21:42:09 +0000

    refactor :: moved theme names into _data to loop over

---
+++ _data/themes.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        id: 'dark',
+        title: 'Dark',
+    },
+    {
+        id: 'camel-sands',
+        title: 'Camel Sands',
+    },
+    {
+        id: 'corn-flower',
+        title: 'Cornflower',
+    },
+    {
+        id: 'blush',
+        title: 'Blush',
+    },
+    {
+        id: 'chocolate-thunda',
+        title: 'Chocolate Thunda',
+    },
+    {
+        id: 'coffee',
+        title: 'Coffee',
+    },
+    {
+        id: 'summer',
+        title: 'Summer',
+    },
+    {
+        id: 'blue',
+        title: 'Blue',
+    }
+];
