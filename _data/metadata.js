a4a688ccd4117b7767b0e64f032c62e8e76119e7
Simon Dann <carbontwelve@users.noreply.github.com>
2022-02-16 08:37:30 +0000

    bugfix :: final tinkering with feeds

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
     "title": "PhotoGabble",
     "subtitle": "Blog and general digital garden of the full stack programmer Simon Dann.",
     "url": "https://photogabble.co.uk",
-    "feedUrl": "https://photogabble.co.uk/feed.xml",
+    "feedUrl": "https://photogabble.co.uk/blog/feed.xml",
     "author": {
         "name": "Simon Dann",
         "email": "simon@photogabble.co.uk"
