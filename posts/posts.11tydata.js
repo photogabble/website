d8dfe1db76e449b7f37b49633b09b0beb35282df
Simon Dann <carbontwelve@users.noreply.github.com>
2022-03-16 21:37:49 +0000

    feat: adds default growthStage for #95

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -2,6 +2,7 @@ module.exports = {
   featured: false,
   draft: false,
   layout: "layouts/post.njk",
+  growthStage: 'seedling', // seedling, budding, evergreen
   eleventyComputed: {
     permalink(data) {
       return `blog/${ data.categories[0] }/${ this.slugify(data.title) }/`
