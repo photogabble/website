74e3289c79795702b6744703eab433f22ddbf7cf
Simon Dann <carbontwelve@users.noreply.github.com>
2022-05-26 23:17:17 +0100

    refactor: moves helpers into one file per function

---
+++ utils/helpers/index.js
@@ -0,0 +1,9 @@
+const chunk = require('./chunk');
+const strToSlug = require('./strToSlug');
+const toTitleCase = require('./toTitleCase');
+
+module.exports = {
+    chunk,
+    strToSlug,
+    toTitleCase
+}
