b311b18844bd5f70c62fff3c972dc9a6a43d4f7a
Simon Dann <carbontwelve@users.noreply.github.com>
2022-06-07 23:33:41 +0100

    feat: adds Netlify Git metadata output to footer

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,14 @@
 module.exports = {
-    "title": "PhotoGabble",
-    "description": "Blog and general digital garden of the full stack programmer Simon Dann.",
-    "url": "https://photogabble.co.uk",
-    "feedUrl": "https://photogabble.co.uk/blog/feed.xml",
-    "author": {
-        "name": "Simon Dann",
-        "email": "simon@photogabble.co.uk"
+    commit: {
+        ref: process.env.COMMIT_REF || null,
+        url: process.env.REPOSITORY_URL
+    },
+    title: "PhotoGabble",
+    description: "Blog and general digital garden of the full stack programmer Simon Dann.",
+    url: "https://photogabble.co.uk",
+    feedUrl: "https://photogabble.co.uk/blog/feed.xml",
+    author: {
+        name: "Simon Dann",
+        email: "simon@photogabble.co.uk"
     }
 }
