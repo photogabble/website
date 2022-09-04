63b7761d239b213d83449ea9242f26390e19c142
Simon Dann <carbontwelve@users.noreply.github.com>
2022-09-04 16:00:29 +0100

    feat: add quote and dad joke lists

---
+++ _data/dadjokes.js
@@ -0,0 +1,11 @@
+module.exports = [
+  'I only know 25 letters of the alphabet. I don\'t know y.',
+  'Why did the invisible man turn down the job offer? He couldn\'t see himself doing it.',
+  'What does a lemon say when it answers the phone?<br/>Yellow!',
+  'Where do bad rainbows go? Prism. It is just a light sentence.',
+  'Why can’t you send a duck to space? Because the bill would be astronomical.',
+  'Want to know why nurses like red crayons? Sometimes they have to draw blood.',
+  'Why did the scarecrow win an award? Because he was the best in his field.',
+  'Why didn’t Han Solo enjoy his steak dinner? It was Chewie.',
+  'When I was a kid, my dad got fired from his job as a road worker for theft. I refused to believe he could do such a thing, but when I got home, the signs were all there.',
+];
---
+++ _data/quotes.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    blockquote: "Everything not saved will be lost",
+    cite: 'Nintendo Quit Screen',
+  },
+  {
+    blockquote: "To use a history of discrimination to deny people their constitutional rights is a perversion of logic and a betrayal of justice.",
+    cite: 'Sarah Grimké',
+  },
+  {
+    blockquote: "Nations that go down fighting rise again, but those who surrender tamely are finished.",
+    cite: 'Churchill',
+  },
+  {
+    blockquote: "The world is changed by your example, not by your opinion",
+    cite: 'Paulo Coelho',
+  },
+  {
+    blockquote: "Thinking is difficult, that's why most people judge",
+    cite: 'Carl Gustav Jung',
+  },
+  {
+    blockquote: "More than cleverness, we need kindness and gentleness",
+    cite: 'Charlie Chaplin',
+  },
+  {
+    blockquote: "Freedom is the only worthy goal in life. It is won by disregarding things that lie beyond our control.",
+    cite: 'Epictetus',
+  },
+  {
+    blockquote: "Human beings are born with different capacities. If they are free, they are not equal. And if they are equal, they are not free.",
+    cite: 'Aleksandr Solzhenitsyn',
+  },
+
+  {
+    blockquote: "Raise your words, not voice. It is rain that grows flowers, not thunder.",
+    cite: 'Rumi',
+  },
+
+  // {
+  //   blockquote: "",
+  //   cite: '',
+  // },
+];
