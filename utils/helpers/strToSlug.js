8d492bc7ee040cc51f75713b280ee4bb8c904374
Simon Dann <carbontwelve@users.noreply.github.com>
2022-09-06 14:11:34 +0100

    bugfix: slugify hash symbol

--- utils/helpers/strToSlug.js
+++ utils/helpers/strToSlug.js
@@ -3,5 +3,5 @@ const slugify = require('slugify');
 module.exports = (string) => slugify(string, {
   lower: true,
   replacement: '-',
-  remove: /[&,+()$~%.'":*?!<>{}]/g,
+  remove: /[&,+()$~%.'":*?!<>{}#]/g,
 });
