b7fafb35d2ac553a87ddf834c71ee321323df86f
Simon <carbontwelve@users.noreply.github.com>
2023-01-28 21:20:28 +0000

    feat(#161): do not crash in invalid wikilinks

--- utils/helpers/wikilinks.js
+++ utils/helpers/wikilinks.js
@@ -1,23 +1,35 @@
 const {slugify} = require("../filters");
+const chalk = require("chalk");
 
-module.exports = function(md, linkMapCache) {
+module.exports = function (md, {linkMapCache, eleventyConfig}) {
+  const deadWikiLinks = new Set();
+
+  eleventyConfig.on('eleventy.after', () => {
+    deadWikiLinks.forEach(
+      slug => console.warn(chalk.blue('[@photogabble/wikilinks]'), chalk.yellow('WARNING'), `WikiLink found pointing to non-existent [${slug}], has been set to default stub.`)
+    );
+  });
+
   // Recognize Mediawiki links ([[text]])
   md.linkify.add("[[", {
     validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
     normalize: match => {
       const parts = match.raw.slice(2, -2).split("|");
       const slug = slugify(parts[0].replace(/.(md|markdown)\s?$/i, "").trim());
-      const found = linkMapCache.get(slug);
+      let found = linkMapCache.get(slug);
 
       if (!found) {
-        throw new Error(`Unable to find page linked by wikilink slug [${slug}]`)
+        deadWikiLinks.add(slug);
+        match.text = parts.length === 2 ? parts[1] : parts[0];
+        match.url = '/stubs';
+        return;
       }
 
       match.text = parts.length === 2
         ? parts[1]
         : found.title;
 
-      match.url = found.permalink.substring(0,1) === '/'
+      match.url = found.permalink.substring(0, 1) === '/'
         ? found.permalink
         : `/${found.permalink}`;
     }
