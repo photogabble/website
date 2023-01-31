93ad0f1ff6951ce79f4a072bd1fefbdbb8806f19
Simon <carbontwelve@users.noreply.github.com>
2023-01-31 22:27:11 +0000

    feat: use @photogabble/eleventy-plugin-blogtimes

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,10 @@ const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(UpgradeHelper);
   eleventyConfig.addPlugin(PostCSSPlugin);
-
+  eleventyConfig.addPlugin(require('@photogabble/eleventy-plugin-blogtimes'), {
+    generateHTML: (outputUrl, options) => `<img alt="Blogtimes histogram" width="${options.width}" height="${options.height}" src="${outputUrl}" style="min-width: auto;" />`,
+    lastXDays: 180,
+  });
   eleventyConfig.setUseGitIgnore(false);
 
   eleventyConfig.addPlugin(wordStats, {
