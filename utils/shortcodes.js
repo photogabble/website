7c0007c4d79c21abf47fe3b12c893a3d06b287a8
Simon <carbontwelve@users.noreply.github.com>
2023-02-09 22:33:35 +0000

    feat: pass 11ty config to shortcodes on construction

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,20 +1,29 @@
-module.exports = {
+module.exports = function loadShortcodes(eleventyConfig) {
+    return {
+        /**
+         * @see https://gist.github.com/dirtystylus/d488ea82fec9ebda8308a288015d019b
+         * @param {string} image
+         * @param {string} caption
+         * @param {string} alt
+         * @param {string} className
+         * @returns {string}
+         */
+        figure: (image, caption, alt, className) => {
+            const classMarkup = className ? ` class="${className}"` : '';
+            const captionMarkup = caption ? `<figcaption>${caption}</figcaption>` : '';
+            const imgMarkup = alt ? `<img src="${image}" alt="${alt}" />` : `<img src="${image}" />`;
+            return `<figure${classMarkup}>${imgMarkup}${captionMarkup}</figure>`;
+        },
 
-    /**
-     * @see https://gist.github.com/dirtystylus/d488ea82fec9ebda8308a288015d019b
-     * @param {string} image
-     * @param {string} caption
-     * @param {string} alt
-     * @param {string} className
-     * @returns {string}
-     */
-    figure: (image, caption, alt, className) => {
-        const classMarkup = className ? ` class="${className}"` : '';
-        const captionMarkup = caption ? `<figcaption>${caption}</figcaption>` : '';
-        const imgMarkup = alt ? `<img src="${image}" alt="${alt}" />` : `<img src="${image}" />`;
-        return `<figure${classMarkup}>${imgMarkup}${captionMarkup}</figure>`;
-    },
+        resource(collection, id) {
 
-    version: () => String(Date.now()),
+            const page = this.page;
 
-}
+            const n = 1;
+            // <pre>{{ content }}</pre>
+            // <small>Source: {{ title }} by {{ cite.name }} | <a href="{{ cite.href }}" title="Visit {{ title }} source by {{ cite.name }}">link</a></small>
+        },
+
+        version: () => String(Date.now()),
+    };
+};
