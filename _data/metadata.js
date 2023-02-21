8e39756cb46715b41c0d2fba670de5049867bf18
Belmin Began <manemoj01@gmail.com>
2023-02-21 17:21:39 +0100

    Feat: Implements correct redirects with migrating feed to writing/feed

--- _data/metadata.js
+++ _data/metadata.js
@@ -6,7 +6,7 @@ module.exports = {
     title: "PhotoGabble",
     description: "Blog and general digital garden of the full stack programmer Simon Dann.",
     url: "https://photogabble.co.uk",
-    feedUrl: "https://photogabble.co.uk/blog/feed.xml",
+    feedUrl: "https://photogabble.co.uk/writing/feed.xml",
     author: {
         name: "Simon Dann",
         email: "simon@photogabble.co.uk"
