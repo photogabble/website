2813d7ec83d7e2f11a4b6fb222c9b8f940ede44c
Simon <carbontwelve@users.noreply.github.com>
2023-03-13 21:27:46 +0000

    feat: add concept of resources and resourceType

--- content/resources/resources.11tydata.js
+++ content/resources/resources.11tydata.js
@@ -3,18 +3,34 @@ const getPageFolders = (page) => page.filePathStem.substring(
   page.filePathStem.indexOf(`/${page.fileSlug}`)
 );
 
+const getPageResourceType = (page) => getPageFolders(page).split('/')[0];
+
 module.exports = {
   layout: "layouts/resource.njk",
-  contentType: 'resource', // thought, noteworthy, essay, tutorial, project, mirror
+  contentType: 'resource',
   excludeFromFeed: true,
   eleventyComputed: {
     permalink(data) {
       const folders = getPageFolders(data.page);
       return `resources/${folders}/${this.slugify(data.title)}/`
     },
-    resourceId(data) {
-      const folders = getPageFolders(data.page);
-      return `${folders}/${this.slugify(data.title)}`;
-    }
+
+    folder(data) {
+      const resourceType = getPageResourceType(data.page);
+
+      return [
+        'resource',
+        {href: `/resources/${resourceType}/`, text: resourceType, title: 'Goto Archive of all resources'}
+      ];
+    },
+
+    /**
+     * Resource Type is the base folder of a resource.
+     * @param data
+     * @returns {string}
+     */
+    resourceType(data) {
+      return getPageResourceType(data.page);
+    },
   }
 };
