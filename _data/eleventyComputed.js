f0e8465b5f6c8336a2fde18c13c8d2fee1b52600
Simon <carbontwelve@users.noreply.github.com>
2023-03-13 21:26:56 +0000

    feat: add resource breadcrumb support

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -8,32 +8,44 @@ module.exports = {
     let folders = data?.folder ?? [];
     if (Array.isArray(folders) === false) folders = [folders];
 
-    if (data?.contentType === 'project') folders = folders.filter(f => f !== 'writing');
+    if (['project', 'resource'].includes(data?.contentType)) folders = folders.filter(f => f !== 'writing');
 
     for (const folder of folders) {
-      switch (folder) {
-        case 'about':
-          crumbs.push({href: '/about', text: 'about', title: 'Goto About page'});
-          break;
-        case 'types':
-          crumbs.push({href: '/about/content', text: 'types', title: 'Goto Content Types info page'});
-          break;
-        case 'writing':
-          crumbs.push({href: '/writing', text: 'writing', title: 'Goto Archive of all posts'});
-          break;
-        case 'topic':
-          crumbs.push({href: '/topic', text: 'topics', title: 'Goto list of all topics'});
-          break;
+      if (typeof folder === 'string') {
+        switch (folder) {
+          case 'about':
+            crumbs.push({href: '/about/', text: 'about', title: 'Goto About page'});
+            break;
+          case 'types':
+            crumbs.push({href: '/about/content/', text: 'types', title: 'Goto Content Types info page'});
+            break;
+          case 'writing':
+            crumbs.push({href: '/writing/', text: 'writing', title: 'Goto Archive of all posts'});
+            break;
+          case 'topic':
+            crumbs.push({href: '/topic/', text: 'topics', title: 'Goto list of all topics'});
+            break;
+          case 'resource':
+            crumbs.push({href: '/resources/', text: 'resources', title: 'Goto Archive of all resources'});
+            break;
+        }
+      } else {
+        crumbs.push(folder);
       }
     }
 
-    if (data.contentType) {
+    // Add link to content type archive page. This is used by writing content pages so I don't have to
+    // set the folder within the computed date for each content type. The exception is resources where
+    // the folder is computed based upon resourceType.
+    if (data.contentType && !data.resourceType) {
       const found = data.collections.contentTypes.find(t => t.id === data.contentType);
-      if (found) crumbs.push({
+      if (!found) return crumbs;
+
+      crumbs.push({
         href: `/${found.slug}`,
         text: found.name.toLowerCase(),
         title: `Goto Archive of all ${found.name}`
-      })
+      });
     }
     return crumbs;
   }
