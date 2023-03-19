58b17bb7c77f8aa241e1f9f06c06a6b016e3a706
Simon <carbontwelve@users.noreply.github.com>
2023-03-19 00:33:07 +0000

    feat: add people and lookup for displaying socials

---
+++ _data/people.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    name: 'Dana Byerly',
+    socials: {
+      github: {
+        href: 'https://github.com/superterrific/',
+        nick: '@superterrific',
+      },
+      mastodon: {
+        href: 'https://mastodon.social/@superterrific',
+        nick: '@superterrific',
+      }
+    }
+  },
+
+  {
+    name: 'Eevis Panula',
+    socials: {
+      github: {
+        href: 'https://github.com/eevajonnapanula',
+        nick: '@eevajonnapanula'
+      }
+    }
+  }
+];
