bb7a78c8c3b9c373faec90e8be51a67eefeeb34d
Simon <carbontwelve@users.noreply.github.com>
2024-01-06 23:36:30 +0000

    feat: add "site-less" buttons page

---
+++ src/_data/buttons.js
@@ -0,0 +1,25 @@
+// This is a list of misc 88x31 buttons that I want to include on my webste but do not require their own
+// bookmark because they do not link to anything.
+module.exports = [
+  // The below four came from https://debtdeath.neocities.org/buttons
+  'debtdeath-neocities.png',
+  'debtdeath-mobilefriendly.png',
+  'debtdeath-html5.png',
+  'debtdeath-css.png',
+
+  // Found via https://exo.pet/
+  'gameboy-advance-net.gif',
+
+  // Found via https://finnisnotafish.com/home.php
+  'dreamweaver.png',
+  'notepadpp.gif',
+
+  // Found via https://billsworld.neocities.org/
+  'msdos.gif',
+
+  // Found via https://personally-comfy.net/
+  'drpepper.gif',
+
+  // Found via https://10kph.neocities.org/
+  'trn.png'
+];
