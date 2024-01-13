bdb5efd6902439a01fafad5dde0e71876e84a1ad
Simon <carbontwelve@users.noreply.github.com>
2024-01-13 11:22:37 +0000

    feat: add intentions page

---
+++ src/_data/intentions.js
@@ -0,0 +1,23 @@
+/*
+ * Based upon https://tiana.computer/sunday_sites/2024.html
+ */
+
+module.exports = [
+  {
+    year: 2024,
+    months: {
+      'jan': 'digital spelunking',
+      'feb': 'relearn & craft pixelart',
+      'mar': 'roguelike gamedev',
+      'apr': '—',
+      'may': '—',
+      'jun': '—',
+      'jul': '—',
+      'aug': 'family',
+      'sep': '—',
+      'oct': '—',
+      'nov': '—',
+      'dec': 'not forcing anything',
+    }
+  }
+];
