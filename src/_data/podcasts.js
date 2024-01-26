c3ada0b760de35d505de6734af1ff713aa7833d5
Simon <carbontwelve@users.noreply.github.com>
2024-01-26 20:30:32 +0000

    feat: add podcast data

---
+++ src/_data/podcasts.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    id: 'developer-tea',
+    type: 'podcast',
+    album: 'Developer Tea',
+    artist: 'Jonathan Cutrell',
+    image: '/img/listening/podcasts/developer-tea.jpeg',
+    url: 'https://developertea.com/',
+    feed: 'https://feeds.simplecast.com/dLRotFGk',
+    description: 'I began listening to Jonathan\'s words of wisdom with his very first episode in 2015. I can\'t say I have listened to them all, but I still dip in from time to time.',
+  },
+  {
+    id: 'accidental-tech-podcast',
+    type: 'podcast',
+    album: 'Accidental Tech Podcast',
+    artist: ['Marco Arment', 'Casey Liss', 'John Siracusa'],
+    image: '/img/listening/podcasts/accidental-tech-podcast.png',
+    url: 'https://atp.fm/',
+    feed: 'https://atp.fm/rss',
+    description: 'A very long time ago I used to chat to Marco on IRC. I stumbled upon this podcast after remembering they built Tumblr and wondering what they were up to now.'
+  },
+  {
+    id: 'infinite-monkey-cage',
+    type: 'podcast',
+    album: 'The Infinite Monkey Cage',
+    artist: ['Brian Cox', 'Robin Ince'],
+    image: '/img/listening/podcasts/infinite-monkey-cage.jpeg',
+    url: 'https://www.bbc.co.uk/programmes/b00snr0w',
+    feed: 'https://podcasts.files.bbci.co.uk/b00snr0w.rss',
+    description: 'Back before I started working from home full time I would occasionally catch this podcast broadcast on Radio 4 during my commute home from work. Since the lockdown I rediscovered it in podcast form.'
+  },
+  {
+    id: 'multimedia-hyperguide',
+    type: 'podcast',
+    album: 'Multimedia HyperGuide:<br/> Windows 3.1, Mac and MS-DOS Gaming in the 90s',
+    artist: 'Super VGA',
+    image: '/img/listening/podcasts/multimedia-hyperguide.png',
+    url: 'https://podcast.vga256.com/',
+    feed: 'https://feeds.feedburner.com/MultimediaHyperguide'
+  },
+];
