2c6c0d5a4c01f1ce6041346ebd5bb60f9cb4b28a
Simon <carbontwelve@users.noreply.github.com>
2024-08-13 08:36:41 +0100

    docs: add references

--- cli.js
+++ cli.js
@@ -2,9 +2,10 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import path from "node:path";
-import fs from "node:fs";
 import { commands } from './bin/index.mjs';
+
+// https://laurieontech.com/posts/yargs/
+// https://github.com/yargs/yargs/blob/main/docs/advanced.md
 
 yargs(hideBin(process.argv))
   .command(commands)
