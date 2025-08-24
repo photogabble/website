afffc532ad5feabd25e434705e2535ffb148281d
Simon <carbontwelve@users.noreply.github.com>
2025-08-24 22:19:00 +0100

    feat: use grouped listing for stage indexes

--- src/content/taxonomy/stage/stage.11tydata.js
+++ src/content/taxonomy/stage/stage.11tydata.js
@@ -3,33 +3,34 @@
  *
  */
 export default {
-  // Do not show related notes
-  show_related: false,
-  // Do not show prev/next links
-  show_prev_next: false,
+    // Do not show related notes
+    show_related: false,
+    // Do not show prev/next links
+    show_prev_next: false,
 
-  // Do not include in RSS Feed
-  excludeFromFeed: true,
+    // Do not include in RSS Feed
+    excludeFromFeed: true,
 
-  // Tagged as special topic type, these aren't regular pages
-  tags: ['type/stage'],
+    // Tagged as special topic type, these aren't regular pages
+    tags: ['type/stage'],
 
-  // Stage is a list of posts
-  // TODO: does this need setting at the taxonomy folder level?
-  layout: 'layouts/page-index.njk',
-  index_navigation: 'growth-stages',
+    // Stage is a list of posts
+    // TODO: does this need setting at the taxonomy folder level?
+    layout: 'layouts/page-index.njk',
+    index_navigation: 'growth-stages',
 
-  // Do not display page metadata
-  hide_meta: true,
-  folder: ['series'],
+    // Do not display page metadata
+    hide_meta: true,
+    folder: ['series'],
 
-  sidebar_component: 'topic',
+    // Listing grouped by year
+    sidebar_component: 'list-table-grouped',
 
-  eleventyComputed: {
-    permalink(data) {
-      return (data.permalink === '')
-        ? `/growth/${data.page.fileSlug}/`
-        : data.permalink;
+    eleventyComputed: {
+        permalink(data) {
+            return (data.permalink === '')
+                ? `/growth/${data.page.fileSlug}/`
+                : data.permalink;
+        },
     },
-  },
 }
