beb9586a629e18b19c07554edae355af61c49e4d
Simon <carbontwelve@users.noreply.github.com>
2025-11-02 20:19:11 +0000

    chore: tidy

--- src/content/taxonomy/lists/lists.11tydata.js
+++ src/content/taxonomy/lists/lists.11tydata.js
@@ -1,19 +1,22 @@
 /**
  * Files within this folder describe my lists and collections. This replaces the legacy lists-meta.js data file.
- * The file slug becomes the list's identifier e.g test.md will reference all posts tagged as `list/test`. This
- * allows writing custom copy for each list as well as easily deciding which layout to use.
+ * The file slug becomes the list's identifier e.g. test.md will reference all posts tagged as `list/test`. This
+ * allows writing a custom copy for each list as well as easily deciding which layout to use.
  */
-import {whereKeyEquals} from "../../../../lib/filters.js";
 
 export default {
   // Do not include in RSS Feed
   excludeFromFeed: true,
-  // Tagged as special topic type, these aren't regular pages
+
+  // Tagged as a special topic type, these aren't regular pages
   tags: ['type/list'],
-  // Do not display page meta data
+
+  // Do not display page metadata
   hide_meta: true,
+
   folder: ['lists'],
-  // Defaulting to grid of bookmarks as most lists will be of this type.
+
+  // Defaulting to a grid of bookmarks as most lists will be of this type.
   sidebar_component: 'bookmark-grid',
 
   // This is used by the lists sidebar component to group lists.
@@ -29,6 +32,7 @@ export default {
         ? `/lists/${data.page.fileSlug}/`
         : data.permalink;
     },
+
     items(data) {
       if (!data.collections.lists) return [];
       const slug = data.list_slug ?? data.page.fileSlug;
